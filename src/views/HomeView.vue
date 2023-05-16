e1cdef32b4a2264a028904ea572cc2d7c36913fb
marekkrzysztofik <marekkrzysztofik2@wp.pl>
2023-05-16 22:20:03 +0200

    start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,25 @@
 </template>
 <script setup lang="ts">
 import * as THREE from 'three'
-import { ref, onMounted } from 'vue'
-
+import { ref, onMounted, computed, watch } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+let renderer: WebGLRenderer
+let camera: PerspectiveCamera
 const experience = ref<HTMLCanvasElement | null>(null)
-const camera = new THREE.PerspectiveCamera(
+const { width, height } = useWindowSize()
+const aspectRatio = computed(() => width.value / height.value)
+const updateRenderer = () => {
+  renderer.setSize(width.value, height.value)
+  renderer.setPixelRatio(window.devicePixelRatio)
+}
+const updateCamera = () => {
+  camera.aspect = aspectRatio.value
+  camera.updateProjectionMatrix()
+}
+watch(aspectRatio, updateRenderer)
+watch(aspectRatio, updateCamera)
+
+camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
@@ -16,19 +31,26 @@ const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(1, 20, 20),
   new THREE.MeshBasicMaterial({ color: 0xff6397 })
 )
+
 camera.position.z = 5
 const scene = new THREE.Scene()
 scene.add(camera)
 scene.add(sphere)
+const loop = () => {
+  renderer.render(scene, camera)
+  sphere.position.y += 0.01;
+  requestAnimationFrame(loop)
+}
 onMounted(() => {
-  const renderer = new THREE.WebGLRenderer({
+  renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
   })
   // const renderer = new THREE.WebGLRenderer({
   //   canvas: experience.value as unknown as HTMLCanvasElement,
   //   antialias: true,
   // })
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.render(scene, camera)
+  updateRenderer()
+  updateCamera()
+  loop()
 })
 </script>
